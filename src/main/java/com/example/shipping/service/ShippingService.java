ca955ce7a6357764aa52f094ead9dc4324410c26
kdayun <rlaekdbs0329@naver.com>
2026-03-20 14:43:38 +0900

    feat: 배송 주문 상세 조회 구현

--- src/main/java/com/example/shipping/service/ShippingService.java
+++ src/main/java/com/example/shipping/service/ShippingService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.example.shipping.domain.dto.ShippingResponseDTO;
+import com.example.shipping.domain.entity.ShippingEntity;
 import com.example.shipping.repository.ShippingRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -18,9 +19,17 @@ public class ShippingService {
     private final ShippingRepository shippingRepository;
 
     public List<ShippingResponseDTO> getList() {
-        log.info("=== Shipping Service List ===");
+        log.info("=== Shipping Service getList ===");
 
         return shippingRepository.findAll().stream().map(ShippingResponseDTO::fromEntity).toList();
     }
 
+    public ShippingResponseDTO getShipping(Integer shipping_id) {
+        log.info("=== Shipping Service getShipping ===");
+
+        ShippingEntity shipping = shippingRepository.findById(shipping_id)
+                .orElseThrow(() -> new RuntimeException("주문 없음"));
+
+        return ShippingResponseDTO.fromEntity(shipping);
+    }
 }
