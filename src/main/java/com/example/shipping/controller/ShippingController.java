f5a61525d793d367151a59990b0b8920191ee26b
Yeongeunn <nye0817@gmail.com>
2026-03-24 16:06:36 +0900

    feat: order-service Feign 연동 및 배송 생성 Kafka 비동기 전환

--- src/main/java/com/example/shipping/controller/ShippingController.java
+++ src/main/java/com/example/shipping/controller/ShippingController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.shipping.domain.dto.ShippingRequestDTO;
 import com.example.shipping.domain.dto.ShippingResponseDTO;
 import com.example.shipping.service.ShippingService;
 
@@ -16,8 +15,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/shippings")
@@ -46,11 +43,10 @@ public class ShippingController {
         return ResponseEntity.ok(shippingService.getShipping(shippingId));
     }
 
-    @PostMapping("/create")
-    @Operation(summary = "배송 주문 생성", description = "QC_PASS 된 주문에 대해 배송 주문 생성하는 API")
-    public ResponseEntity<ShippingResponseDTO> create(@RequestBody ShippingRequestDTO request) {
-        log.info("=== Shipping Controller create ===");
-        return ResponseEntity.ok(shippingService.create(request));
-    }
+    // 배송 생성은 Kafka(shipping.ready) 비동기로만 처리 - simulator-service에서 QC_PASS 시 발행
+    // @PostMapping("/create")
+    // public ResponseEntity<ShippingResponseDTO> create(@RequestBody ShippingRequestDTO request) {
+    //     return ResponseEntity.ok(shippingService.create(request));
+    // }
 
 }
